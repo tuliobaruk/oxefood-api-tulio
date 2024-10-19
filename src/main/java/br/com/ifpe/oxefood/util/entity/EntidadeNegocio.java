d33162af661bcf1a43c17ffec315fba64b1cb115
limpolimpolimpo <limpolimpolimpo>
2024-10-19 11:01:22 -0300

    feat: entidades base

---
+++ src/main/java/br/com/ifpe/oxefood/util/entity/EntidadeNegocio.java
@@ -0,0 +1,17 @@
+package br.com.ifpe.oxefood.util.entity;
+
+import java.io.Serializable;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(of = { "id" })
+public abstract class EntidadeNegocio implements Serializable {
+
+    private Long id;
+    private Boolean habilitado;
+    
+}
