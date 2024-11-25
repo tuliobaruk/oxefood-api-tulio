21cdf7600ce720bf0a329865591098ea90cea145
tuliobaruk <tuliobarukms@gmail.com>
2024-11-25 12:36:45 -0300

    fix: Adição da anotation @Service para que o CategoriaProdutoService realmente funcione como o esperado

--- src/main/java/br/com/ifpe/oxefood/modelo/produto/CategoriaProdutoService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/produto/CategoriaProdutoService.java
@@ -4,8 +4,11 @@ import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import jakarta.transaction.Transactional;
 
+@Service
 public class CategoriaProdutoService {
     @Autowired
     private CategoriaProdutoRepository repository;
