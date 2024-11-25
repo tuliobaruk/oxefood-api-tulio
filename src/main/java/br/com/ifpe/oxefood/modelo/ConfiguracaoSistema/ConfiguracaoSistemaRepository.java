d6fa7d6541f2b9455f9f82b53bedd3d03af8afee
tuliobaruk <tuliobarukms@gmail.com>
2024-11-25 12:16:01 -0300

    remove: Removendo entidade de configuração do sistema

--- src/main/java/br/com/ifpe/oxefood/modelo/ConfiguracaoSistema/ConfiguracaoSistemaRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.ifpe.oxefood.modelo.ConfiguracaoSistema;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ConfiguracaoSistemaRepository extends JpaRepository<ConfiguracaoSistema, Long> {
-    
-}
