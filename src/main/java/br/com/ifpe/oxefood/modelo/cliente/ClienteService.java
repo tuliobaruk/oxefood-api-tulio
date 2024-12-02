8a4b4c909aa5b7bde65a8a628cbcea87df2bc305
tuliobaruk <tuliobarukms@gmail.com>
2024-12-02 11:00:37 -0300

    feat: Adicionando exceção ao dar find em um Cliente inexistente

--- src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
@@ -2,9 +2,12 @@ package br.com.ifpe.oxefood.modelo.cliente;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.ifpe.oxefood.util.exception.EntidadeNaoEncontradaException;
 import jakarta.transaction.Transactional;
 
 @Service
@@ -32,7 +35,13 @@ public class ClienteService {
 
     public Cliente obterPorID(Long id) {
 
-        return repository.findById(id).get();
+        Optional<Cliente> consulta = repository.findById(id);
+
+        if (consulta.isPresent()) {
+            return consulta.get();
+        } else {
+            throw new EntidadeNaoEncontradaException("Cliente", id);
+        }
     }
 
     @Transactional
