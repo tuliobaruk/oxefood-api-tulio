fcf999bac65034a8c3b7e618b7f3c3ed64907083
tuliobaruk <tuliobarukms@gmail.com>
2024-12-16 11:32:33 -0300

    fix: Uma barra errada pemitia que um Cliente cadastrasse indevidamente um Produto

--- src/main/java/br/com/ifpe/oxefood/config/SecurityConfiguration.java
+++ src/main/java/br/com/ifpe/oxefood/config/SecurityConfiguration.java
@@ -23,74 +23,71 @@ import br.com.ifpe.oxefood.modelo.seguranca.JwtAuthenticationFilter;
 @EnableWebSecurity
 public class SecurityConfiguration {
 
-    private final AuthenticationProvider authenticationProvider;
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+        private final AuthenticationProvider authenticationProvider;
+        private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    public SecurityConfiguration(JwtAuthenticationFilter jwtAuthenticationFilter,
-            AuthenticationProvider authenticationProvider) {
-        this.authenticationProvider = authenticationProvider;
-        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
-    }
+        public SecurityConfiguration(JwtAuthenticationFilter jwtAuthenticationFilter,
+                        AuthenticationProvider authenticationProvider) {
+                this.authenticationProvider = authenticationProvider;
+                this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        }
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http
-                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .csrf(c -> c.disable())
-                .authorizeHttpRequests(authorize -> authorize
+                http
+                                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                                .csrf(c -> c.disable())
+                                .authorizeHttpRequests(authorize -> authorize
 
-                        .requestMatchers(HttpMethod.POST, "/api/cliente").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/funcionario").permitAll()
-                        .requestMatchers(HttpMethod.POST, "/api/auth").permitAll()
+                                                .requestMatchers(HttpMethod.POST, "/api/cliente").permitAll()
+                                                .requestMatchers(HttpMethod.POST, "/api/funcionario").permitAll()
+                                                .requestMatchers(HttpMethod.POST, "/api/auth").permitAll()
 
-                        .requestMatchers(HttpMethod.GET, "/api-docs/*").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/swagger-ui/*").permitAll()
+                                                .requestMatchers(HttpMethod.GET, "/api/produto/").hasAnyAuthority(
+                                                                Perfil.ROLE_CLIENTE,
+                                                                Perfil.ROLE_FUNCIONARIO_ADMIN,
+                                                                Perfil.ROLE_FUNCIONARIO_USER) // Consulta de produto
 
-                        .requestMatchers(HttpMethod.GET, "/api/categoriaProduto/")
-                        .hasAnyAuthority(Perfil.ROLE_CLIENTE, Perfil.ROLE_FUNCIONARIO_ADMIN,
-                                Perfil.ROLE_FUNCIONARIO_USER)
+                                                .requestMatchers(HttpMethod.GET, "/api/categoriaproduto").permitAll()
+                                                .requestMatchers(HttpMethod.GET, "/api/categoriaproduto/*").permitAll()
 
-                        .requestMatchers(HttpMethod.GET, "/api/produto/")
-                        .hasAnyAuthority(Perfil.ROLE_CLIENTE, Perfil.ROLE_FUNCIONARIO_ADMIN,
-                                Perfil.ROLE_FUNCIONARIO_USER) // Consulta de produto
+                                                .requestMatchers(HttpMethod.POST, "/api/produto").hasAnyAuthority(
+                                                                Perfil.ROLE_FUNCIONARIO_ADMIN,
+                                                                Perfil.ROLE_FUNCIONARIO_USER) // Cadastro de produto
 
-                        .requestMatchers(HttpMethod.POST, "/api/produto/")
-                        .hasAnyAuthority(Perfil.ROLE_FUNCIONARIO_ADMIN, Perfil.ROLE_FUNCIONARIO_USER) // Cadastro de produto
+                                                .requestMatchers(HttpMethod.PUT, "/api/produto/*").hasAnyAuthority(
+                                                                Perfil.ROLE_FUNCIONARIO_ADMIN,
+                                                                Perfil.ROLE_FUNCIONARIO_USER) // Alteração de produto
 
-                        .requestMatchers(HttpMethod.POST, "/api/categoriaProduto/")
-                        .hasAnyAuthority(Perfil.ROLE_FUNCIONARIO_ADMIN,
-                                Perfil.ROLE_FUNCIONARIO_USER)
+                                                .requestMatchers(HttpMethod.DELETE, "/api/produto/*").hasAnyAuthority(
+                                                                Perfil.ROLE_FUNCIONARIO_ADMIN) // Exclusão de produto
 
-                        .requestMatchers(HttpMethod.PUT, "/api/produto/*")
-                        .hasAnyAuthority(Perfil.ROLE_FUNCIONARIO_ADMIN, Perfil.ROLE_FUNCIONARIO_USER) // Alteração de
-                                                                                                        // produto
-                                                                                                        
-                        .requestMatchers(HttpMethod.DELETE, "/api/produto/*")
-                        .hasAnyAuthority(Perfil.ROLE_FUNCIONARIO_ADMIN) // Exclusão de produto
+                                                .requestMatchers(HttpMethod.GET, "/api-docs/*").permitAll()
+                                                .requestMatchers(HttpMethod.GET, "/swagger-ui/*").permitAll()
 
-                        .anyRequest().authenticated()
+                                                .anyRequest().authenticated()
 
-                )
-                .sessionManagement((session) -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                                )
+                                .sessionManagement((session) -> session
+                                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                                .authenticationProvider(authenticationProvider)
+                                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
-        return http.build();
-    }
+                return http.build();
+        }
 
-    public CorsConfigurationSource corsConfigurationSource() {
+        public CorsConfigurationSource corsConfigurationSource() {
 
-        CorsConfiguration configuration = new CorsConfiguration();
+                CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
-        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
-        configuration.setAllowCredentials(true);
+                configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+                configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+                configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+                configuration.setAllowCredentials(true);
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
+                UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+                source.registerCorsConfiguration("/**", configuration);
+                return source;
+        }
 }
